09844ca4a57fe79ef24ed0690b5936ef029fbdb8
alexorn19 <alexanderhannesson@gmail.com>
2019-12-07 20:57:56 +0000

    os

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, flash, redirect, url_for, session, request, logging
+from flask import Flask, render_template, flash, redirect, url_for, session, request, logging, os
 #from data import Articles
 from flask_mysqldb import MySQL
 from wtforms import Form, StringField, TextAreaField, PasswordField, validators
